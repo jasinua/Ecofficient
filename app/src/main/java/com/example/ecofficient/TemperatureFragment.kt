e9d597d748e45245d190483d3ad6489599fc103f
jasinua <115897385+jasinua@users.noreply.github.com>
2024-05-25 09:11:09 +0200

    All aaas

--- app/src/main/java/com/example/ecofficient/TemperatureFragment.kt
+++ app/src/main/java/com/example/ecofficient/TemperatureFragment.kt
@@ -24,6 +24,23 @@ class TemperatureFragment : Fragment() {
 
         val dbRef = FirebaseDatabase.getInstance().getReference("Sensor")
 
+        dbRef.child("humidity_data").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+
+
+                view.findViewById<TextView>(R.id.humidity).text = "Humidity: " + snapshot.getValue().toString() + "%"
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
+        var x = 0
+
         dbRef.child("temperature_data").addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
@@ -34,7 +51,25 @@ class TemperatureFragment : Fragment() {
                     view.findViewById<TextView>(R.id.getTemperature4).text = snapshot.child("3").getValue().toString() + " °C"
                     view.findViewById<TextView>(R.id.getTemperature5).text = snapshot.child("4").getValue().toString() + " °C"
 
+
                     view.findViewById<TextView>(R.id.averageTemperature).text = snapshot.child("avg").getValue().toString() + " °C"
+
+                    view.findViewById<TextView>(R.id.averageTemperature).setOnClickListener {
+
+                        var y =  snapshot.child("avg").getValue().toString().toDouble()
+                        if (x%2 == 0) {
+                            y = ((9/5)* y) + 32
+                            view.findViewById<TextView>(R.id.averageTemperature).text = "" + y + "°F"
+
+                        } else {
+                            y = snapshot.child("avg").getValue().toString().toDouble()
+                            view.findViewById<TextView>(R.id.averageTemperature).text = "" + y + "°C "
+                        }
+
+                        x++
+
+
+                    }
 
             }
 
