2fd7176624845cea9c77d7e98512a6bfa0f17283
ChainlessSlave <151469142+ChainlessSlave@users.noreply.github.com>
2024-06-09 20:05:31 +0200

    DoorControl added
    E kom shtu ni sen qe me kontrollu me mshel deren edhe me qel prej telit

--- app/src/main/java/com/example/ecofficient/HomePage.kt
+++ app/src/main/java/com/example/ecofficient/HomePage.kt
@@ -7,36 +7,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.navigation.Navigation
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class HomePage : Fragment() {
 
-
+    private lateinit var databaseReference: DatabaseReference
 
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home_page, container, false)
 
+        databaseReference = FirebaseDatabase.getInstance().getReference("OpenOrClosed")
 
-        view.findViewById<CardView>(R.id.cardViewLightSensor).setOnClickListener{
+        view.findViewById<CardView>(R.id.cardViewLightSensor).setOnClickListener {
 
             Navigation.findNavController(view).navigate(R.id.action_homePage_to_lightSensorFragment)
 
         }
 
-
-        view.findViewById<CardView>(R.id.cardViewTemperaure).setOnClickListener{
+        view.findViewById<CardView>(R.id.cardViewTemperaure).setOnClickListener {
 
             Navigation.findNavController(view).navigate(R.id.action_homePage_to_temperatureFragment)
 
         }
 
+        var i = 1
+        view.findViewById<CardView>(R.id.doorControl).setOnClickListener {
 
+            i += 1
+            if (i % 2 == 0) {
+                databaseReference.child("?").setValue("0").addOnCompleteListener {
+
+                }.addOnFailureListener {
+                    Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                databaseReference.child("?").setValue("1").addOnCompleteListener {
+
+                }.addOnFailureListener(){
+                    Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
         return view;
     }
--- app/src/main/java/com/example/ecofficient/TemperatureFragment.kt
+++ app/src/main/java/com/example/ecofficient/TemperatureFragment.kt
@@ -15,73 +15,64 @@ class TemperatureFragment : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_temperature, container, false)
 
-
         val dbRef = FirebaseDatabase.getInstance().getReference("Sensor")
 
-        dbRef.child("humidity_data").addValueEventListener(object: ValueEventListener {
+        dbRef.child("humidity_data").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
 
-
-                view.findViewById<TextView>(R.id.humidity).text = "Humidity: " + snapshot.getValue().toString() + "%"
+                view.findViewById<TextView>(R.id.humidity).text =
+                    "Humidity: " + snapshot.getValue().toString() + "%"
 
             }
-
             override fun onCancelled(error: DatabaseError) {
-
             }
-
         })
 
         var x = 0
 
-        dbRef.child("temperature_data").addValueEventListener(object: ValueEventListener {
+        dbRef.child("temperature_data").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
 
-                    view.findViewById<TextView>(R.id.getTemperature1).text = snapshot.child("0").getValue().toString() + " °C"
-                    view.findViewById<TextView>(R.id.getTemperature2).text = snapshot.child("1").getValue().toString() + " °C"
-                    view.findViewById<TextView>(R.id.getTemperature3).text = snapshot.child("2").getValue().toString() + " °C"
-                    view.findViewById<TextView>(R.id.getTemperature4).text = snapshot.child("3").getValue().toString() + " °C"
-                    view.findViewById<TextView>(R.id.getTemperature5).text = snapshot.child("4").getValue().toString() + " °C"
+                view.findViewById<TextView>(R.id.getTemperature1).text =
+                    snapshot.child("0").getValue().toString() + " °C"
+                view.findViewById<TextView>(R.id.getTemperature2).text =
+                    snapshot.child("1").getValue().toString() + " °C"
+                view.findViewById<TextView>(R.id.getTemperature3).text =
+                    snapshot.child("2").getValue().toString() + " °C"
+                view.findViewById<TextView>(R.id.getTemperature4).text =
+                    snapshot.child("3").getValue().toString() + " °C"
+                view.findViewById<TextView>(R.id.getTemperature5).text =
+                    snapshot.child("4").getValue().toString() + " °C"
 
 
-                    view.findViewById<TextView>(R.id.averageTemperature).text = snapshot.child("avg").getValue().toString() + " °C"
+                view.findViewById<TextView>(R.id.averageTemperature).text =
+                    snapshot.child("avg").getValue().toString() + " °C"
 
-                    view.findViewById<TextView>(R.id.averageTemperature).setOnClickListener {
+                view.findViewById<TextView>(R.id.averageTemperature).setOnClickListener {
 
-                        var y =  snapshot.child("avg").getValue().toString().toDouble()
-                        if (x%2 == 0) {
-                            y = ((9/5)* y) + 32
-                            view.findViewById<TextView>(R.id.averageTemperature).text = "" + y + "°F"
+                    var y = snapshot.child("avg").getValue().toString().toDouble()
+                    if (x % 2 == 0) {
+                        y = ((9 / 5) * y) + 32
+                        view.findViewById<TextView>(R.id.averageTemperature).text = "" + y + "°F"
 
-                        } else {
-                            y = snapshot.child("avg").getValue().toString().toDouble()
-                            view.findViewById<TextView>(R.id.averageTemperature).text = "" + y + "°C "
-                        }
-
-                        x++
-
-
+                    } else {
+                        y = snapshot.child("avg").getValue().toString().toDouble()
+                        view.findViewById<TextView>(R.id.averageTemperature).text = "" + y + "°C "
                     }
+                    x++
+                }
 
             }
-
             override fun onCancelled(error: DatabaseError) {
-
             }
-
         })
-
-
-
         return view;
     }
-
 }
